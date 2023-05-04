af912a92183de7383e00f1c478c2be7e77810fdf
DevAntonioRogers <ar919061493@gmail.com>
2023-05-04 11:18:47 -0400

    Accessible Carousel

--- app/component/Carousel.tsx
+++ app/component/Carousel.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
@@ -7,9 +7,11 @@ function App() {
   const slides = [
     {
       url: "https://i.ibb.co/qDDtKdw/Slide1.jpg",
+      alt: "First slide",
     },
     {
       url: "https://i.ibb.co/z2m022Z/Adobe-Stock-246868093.jpg",
+      alt: "Second slide",
     },
   ];
 
@@ -27,20 +29,50 @@ function App() {
     setCurrentIndex(newIndex);
   };
 
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "ArrowLeft") {
+        prevSlide();
+      } else if (event.key === "ArrowRight") {
+        nextSlide();
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [prevSlide, nextSlide]);
+
   return (
-    <div className="max-w-[1400px] h-[500px] w-full m-auto relative group">
+    <div
+      className="max-w-[1400px] h-[500px] w-full m-auto relative group focus:outline-primary focus:rounded-xl"
+      role="region"
+      aria-label="Image slideshow"
+      tabIndex="0"
+    >
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+        alt={slides[currentIndex].alt}
       ></div>
       {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <ChevronLeftIcon onClick={prevSlide} />
-      </div>
+      <button
+        className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+        onClick={prevSlide}
+        aria-label="Previous slide"
+        tabIndex="0"
+      >
+        <ChevronLeftIcon />
+      </button>
       {/* Right Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <ChevronRightIcon onClick={nextSlide} />
-      </div>
+      <button
+        className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+        onClick={nextSlide}
+        aria-label="Next slide"
+        tabIndex="0"
+      >
+        <ChevronRightIcon />
+      </button>
     </div>
   );
 }
