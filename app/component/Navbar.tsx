48ce681ffd4ae44ae1e48fad8d5ad43ff15f175b
DevAntonioRogers <ar919061493@gmail.com>
2023-05-06 00:29:31 -0400

    Rearranged Nav Code

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,8 +1,9 @@
 "use client";
+
+import { useState } from "react";
 import { Session } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 
 // ICONS
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
@@ -29,13 +30,54 @@ const Navbar = ({ user }: Session) => {
         <h1 className="text-2xl font-bold text-primary italic whitespace-nowrap">PREMIUM CAPS</h1>
       </Link>
       {/* DESKTOP MENU */}
-      <ul className="md:flex gap-8 text-gray-600 hidden md:block absolute md:static bg-white top-12 right-5 p-5 z-10">
+      <ul className="md:flex gap-8 text-gray-600 hidden md:visible absolute md:static bg-white top-12 right-5 p-5 z-10">
         {menuLinks.map((link) => (
           <li>
             <a href={link.href}>{link.name}</a>
           </li>
         ))}
       </ul>
+      <div className="hidden md:block">
+        {!user && (
+          <div className="flex gap-4 items-center">
+            <Link href={"./api/auth/signin"}>
+              <button className="bg-primary py-1 px-4 rounded-md text-white">Log In</button>
+            </Link>
+            <Link href={"./api/auth/signin"}>
+              <button className="bg-secondary py-1 px-4 rounded-md text-white">Sign Up</button>
+            </Link>
+          </div>
+        )}
+        {user && (
+          <div className="flex items-center gap-6">
+            <div className="text-gray-600 flex items-center gap-6 [&>*:nth-child(2)]:text-red-600 cursor-pointer">
+              <ShoppingCartIcon />
+              <FavoriteIcon />
+            </div>
+            <Image
+              src={user?.image as string}
+              alt={user.name as string}
+              width={45}
+              height={45}
+              onClick={() => setUserMenu(!userMenu)}
+              className="rounded-full cursor-pointer outline outline-primary"
+              tabIndex={0}
+            />
+
+            {userMenu && (
+              <ul className="z-10 absolute right-12 top-20 w-48 bg-white shadow-md rounded-md p-4">
+                <Link href={"./api/auth/signout"}>
+                  <li>Sign Out</li>
+                </Link>
+                <li>Favorites</li>
+                <li>Profile</li>
+              </ul>
+            )}
+          </div>
+        )}
+      </div>
+      {/* DESKTOP MENU END */}
+
       {/* MOBILE MENU ICONS */}
       <div className="flex gap-4 md:hidden">
         <div className="text-gray-600">
@@ -61,7 +103,7 @@ const Navbar = ({ user }: Session) => {
           ))}
           {user && (
             <>
-              <div className="flex justify-center items-center gap-4">
+              <div className="flex items-center gap-4">
                 <Image
                   src={user?.image as string}
                   alt={user?.name as string}
@@ -91,47 +133,7 @@ const Navbar = ({ user }: Session) => {
           )}
         </nav>
       )}
-
-      <div className="hidden md:block">
-        {!user && (
-          <div className="flex gap-4 items-center">
-            <Link href={"./api/auth/signin"}>
-              <button className="bg-primary py-1 px-4 rounded-md text-white">Log In</button>
-            </Link>
-            <Link href={"./api/auth/signin"}>
-              <button className="bg-secondary py-1 px-4 rounded-md text-white">Sign Up</button>
-            </Link>
-          </div>
-        )}
-
-        {user && (
-          <div className="flex items-center gap-6">
-            <div className="text-gray-600 flex items-center gap-6 [&>*:nth-child(2)]:text-red-600 cursor-pointer">
-              <ShoppingCartIcon />
-              <FavoriteIcon />
-            </div>
-            <Image
-              src={user?.image as string}
-              alt={user.name as string}
-              width={45}
-              height={45}
-              onClick={() => setUserMenu(!userMenu)}
-              className="rounded-full cursor-pointer outline outline-primary"
-              tabIndex={0}
-            />
-
-            {userMenu && (
-              <ul className="z-10 absolute right-12 top-20 w-48 bg-white shadow-md rounded-md p-4">
-                <Link href={"./api/auth/signout"}>
-                  <li>Sign Out</li>
-                </Link>
-                <li>Favorites</li>
-                <li>Profile</li>
-              </ul>
-            )}
-          </div>
-        )}
-      </div>
+      {/* MOBILE MENU END */}
     </nav>
   );
 };
