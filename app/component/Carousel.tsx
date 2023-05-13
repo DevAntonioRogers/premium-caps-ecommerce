b83cc0961149cec6a1618f7cff0e7de592a0c2f5
DevAntonioRogers <ar919061493@gmail.com>
2023-05-13 19:01:57 -0400

    Grid change

--- app/component/Carousel.tsx
+++ app/component/Carousel.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Carousel = () => {
   return (
-    <div className="relative grid md:grid-rows-2 md:grid-cols-4 grid-cols-1 grid-rows-none gap-4 md:max-h-[500px] h-full ">
+    <div className="relative grid md:grid-rows-2 md:grid-cols-4 grid-cols-2  grid-rows-none gap-2 md:max-h-[500px] h-full ">
       <div className="md:row-start-1 md:col-start-1 md:row-end-2 md:col-end-2 md:h-full h-[300px]">
         <Image className="h-full w-full object-cover" src={tests} alt="test" width={0} height={0} />
       </div>
@@ -18,7 +18,7 @@ const Carousel = () => {
         <Image className="h-full w-full object-cover" src={tests} alt="test" width={0} height={0} />
       </div>
 
-      <div className=" hidden md:block md:row-start-2 md:col-start-4 md:row-end-3 md:col-end-5">
+      <div className="md:row-start-2 md:col-start-4 md:row-end-3 md:col-end-5">
         <Image className="h-full w-full object-cover" src={tests} alt="test" width={0} height={0} />
       </div>
 
