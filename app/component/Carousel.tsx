ce5272e8ee2ca8a1e3ecb07b44a71f884c707fae
DevAntonioRogers <ar919061493@gmail.com>
2023-05-15 11:51:51 -0400

    Changed Grid

--- app/component/Carousel.tsx
+++ app/component/Carousel.tsx
@@ -1,29 +1,32 @@
 import tests from "@/public/test.jpeg";
 import testss from "@/public/testsss.png";
 import testers from "@/public/TESTERSS.png";
+
+import mens from "@/public/gridimage1.jpeg";
+import womans from "@/public/gridimage2.jpeg";
 import Image from "next/image";
 
 const Carousel = () => {
   return (
-    <div className="relative grid md:grid-rows-2 md:grid-cols-4 grid-cols-2  grid-rows-none gap-2 md:max-h-[500px] h-full ">
-      <div className="md:row-start-1 md:col-start-1 md:row-end-2 md:col-end-2 md:h-full h-[300px]">
+    <div className="relative grid md:grid-rows-2 md:grid-cols-4 grid-cols-2  grid-rows-none gap-2 md:max-h-[500px] h-full">
+      {/* <div className="md:row-start-1 md:col-start-1 md:row-end-2 md:col-end-2 md:h-full h-[300px]">
         <Image className="h-full w-full object-cover" src={tests} alt="test" width={0} height={0} />
-      </div>
+      </div> */}
 
       <div className=" relative md:row-start-1 md:col-start-2 md:row-end-3 md:col-end-4 md:h-full h-[300px]">
         <Image className="h-full w-full object-cover" src={testers} alt="test" width={0} height={0} />
       </div>
 
-      <div className="md:col-start-4 md:row-end-2 md:col-end-5 md:h-full h-[300px]">
+      {/* <div className="md:col-start-4 md:row-end-2 md:col-end-5 md:h-full h-[300px]">
         <Image className="h-full w-full object-cover" src={tests} alt="test" width={0} height={0} />
+      </div> */}
+
+      <div className="md:row-start-1 md:col-start-4 md:row-end-3 md:col-end-5">
+        <Image className="h-full w-full object-cover" src={mens} alt="test" width={0} height={0} />
       </div>
 
-      <div className="md:row-start-2 md:col-start-4 md:row-end-3 md:col-end-5">
-        <Image className="h-full w-full object-cover" src={tests} alt="test" width={0} height={0} />
-      </div>
-
-      <div className="hidden md:block md:row-start-2 md:col-start-1 md:row-end-3 md:col-end-2">
-        <Image className="h-full w-full object-cover" src={tests} alt="test" width={0} height={0} />
+      <div className="hidden md:block md:row-start-1 md:col-start-1 md:row-end-3 md:col-end-2">
+        <Image className="h-full w-full object-cover" src={womans} alt="test" width={0} height={0} />
       </div>
     </div>
   );
