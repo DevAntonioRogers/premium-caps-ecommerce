110b94fc1b21833b06239e6866f5da1aed69a148
DevAntonioRogers <ar919061493@gmail.com>
2023-06-16 01:31:12 -0400

    Footer Styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,28 +14,28 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const Footer = () => {
   return (
-    <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 items-center gap-4 h-60">
-      <div className="flex flex-col gap-3 h-full justify-center">
-        <h1 className="text-2xl font-bold text-primary underline self-center justify-self-start">About</h1>
+    <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 items-stretch gap-4 h-auto">
+      <div className="flex flex-col gap-6 justify-center items-center h-full">
+        <h1 className="text-2xl font-bold text-primary underline ">About</h1>
 
-        <div className="flex flex-col gap-1">
-          <p>Founded in 1886 Premuim Caps devours other hat stores. With 100% Qaulity and customer satifaction!</p>
+        <div className="flex flex-col justify-center gap-1 items-center text-center">
+          <p>Founded in 1886 Premium Caps devours other hat stores.</p>
 
-          <div className="flex items-center gap-3">
+          <div className="flex items-center justify-center gap-3">
             <Image className="object-cover" src={amexCard} height={40} width={40} alt="amex" />
             <Image className="object-cover" src={mastercard} height={40} width={40} alt="mastercard" />
             <Image className="object-cover" src={paypalCard} height={40} width={40} alt="paypal" />
             <Image className="object-cover" src={visaCard} height={40} width={40} alt="visa" />
           </div>
-          <div className="flex items-center gap-1">
+          <div className="flex items-center gap-1 justify-center">
             <Image className="object-cover" src={lock} height={30} width={30} alt="secure payments" />
             <h3>Secure Online Payments</h3>
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col gap-3 justify-center items-center h-full">
-        <h1 className="text-2xl font-bold  text-primary underline">Menu</h1>
+      <div className="flex flex-col gap-6 justify-center items-center h-full">
+        <h1 className="text-2xl font-bold text-primary underline">Menu</h1>
         <div>
           <ul className="flex flex-col gap-1">
             <a href="/">
@@ -57,8 +57,8 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex flex-col gap-3 justify-center items-center h-full">
-        <h1 className="text-2xl font-bold  text-primary underline">Important</h1>
+      <div className="flex flex-col gap-6 justify-center items-center h-full">
+        <h1 className="text-2xl font-bold text-primary underline">Important</h1>
         <ul className="flex flex-col gap-1">
           <a href="/">
             <li>Terms of Service</li>
@@ -78,11 +78,17 @@ const Footer = () => {
         </ul>
       </div>
 
-      <div className="flex flex-col gap-3 justify-center items-center h-full">
-        <h1 className="text-2xl font-bold  text-primary underline">Contact</h1>
+      <div className="flex flex-col gap-6 justify-center items-center h-full">
+        <h1 className="text-2xl font-bold text-primary underline">Contact</h1>
         <div>
           <h3>
             Address:<span>145 State Street Raleigh, NC 27610</span>
+          </h3>
+          <h3>
+            Phone:<span>188-888-8888</span>
+          </h3>
+          <h3>
+            Email:<span>sales@premiumcaps.com</span>
           </h3>
           <h3>
             Phone:<span>188-888-8888</span>
