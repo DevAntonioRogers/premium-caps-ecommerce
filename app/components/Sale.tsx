4a2baa4ca0cc138bea60b4c3e312bcc025a80c76
DevAntonioRogers <ar919061493@gmail.com>
2023-06-23 21:55:20 -0400

    Styled Sale Section

--- app/components/Sale.tsx
+++ app/components/Sale.tsx
@@ -5,23 +5,24 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import AddtoWishlistButton from "../UI/AddToWishlistButton";
 
-const MenCollection = async () => {
+const Sale = async () => {
   const products = await FetchProducts();
   const session = await getServerSession(authOptions);
-  const menProducts = products.filter((product) => product.metadata.collection === "men");
+  const menProducts = products.filter((product) => product.metadata.sale === "true");
 
   return (
     <div className="w-full my-10">
       <div className="w-[92%] m-auto lg:max-w-[1500px]">
-        <h1 className="text-center text-2xl font-bold text-primary underline mb-5">Men Collection</h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
+        <h1 className="text-center text-2xl font-bold mb-5 ">Shop Sale</h1>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5">
           {menProducts.map((product) => (
             <div
               key={product.id}
-              className="bg-background shadow-lg flex flex-col items-center justify-center rounded-md "
+              className="bg-background shadow-lg flex flex-col items-center justify-center rounded-md relative"
             >
+              <span className="absolute left-5 top-5 bg-black text-white px-3 rounded-lg uppercase">Sale</span>
               <Product {...product} />
-              <div className="flex  md:mt-0 mt-2 gap-1 md:gap-3 flex-col md:flex-row">
+              <div className="flex md:mt-0 mt-2 gap-1 lg:gap-3 flex-col md:flex-row flex-wrap justify-center">
                 <AddToCartButton {...product} user={session?.user} />
                 <AddtoWishlistButton {...product} user={session?.user} />
               </div>
@@ -33,4 +34,4 @@ const MenCollection = async () => {
   );
 };
 
-export default MenCollection;
+export default Sale;
--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,15 @@ import Blog from "./components/Blog";
 import Hero from "./components/Hero";
 import Icons from "./components/Icons";
 import MenCollection from "./components/MenCollection";
-import Products from "./components/Products";
-import Tabs from "./components/Tabs";
-import Testy from "./components/Testy";
-import WomenCollection from "./components/WomanCollection";
+import Sale from "./components/Sale";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Icons />
-      <MenCollection />
-
+      <Sale />
       <Banner />
-
       <MenCollection />
       <MenCollection />
       <Blog />
